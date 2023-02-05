872a83ad762bb5c600368a9aae245e0f031b4b16
kucicm <10778553+KucicM@users.noreply.github.com>
2023-02-05 21:13:22 +0100

    ex16

---
+++ ex16.sql
@@ -0,0 +1,34 @@
+/*
+Countries where there are customers 
+Show a list of countries where the Northwind company has customers. 
+*/
+
+SELECT DISTINCT Country
+FROM public.Customers;
+
+/*
+   country   
+-------------
+ Argentina
+ Spain
+ Switzerland
+ Italy
+ Venezuela
+ Belgium
+ Norway
+ Sweden
+ USA
+ France
+ Mexico
+ Brazil
+ Austria
+ Poland
+ UK
+ Ireland
+ Germany
+ Denmark
+ Canada
+ Finland
+ Portugal
+(21 rows)
+*/
