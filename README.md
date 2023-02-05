31a331c414c3f6914c34eea08826e4c6bf18640a
kucicm <10778553+KucicM@users.noreply.github.com>
2023-02-05 20:33:04 +0100

    ex1

--- README.md
+++ README.md
@@ -13,8 +13,10 @@ docker run --name pg-docker -e POSTGRES_PASSWORD=password -d -p 5432:5432 postgr
 
 ## Load data to database
 ``` bash
-psql -h localhost -U postgres -d postgres < northwind2016.sql
+PGPASSWORD=password psql -h localhost -U postgres -d postgres < northwind2016.sql
 ```
 
-use password: password
-
+## run exX.sql
+``` bash
+PGPASSWORD=password psql -h localhost -U postgres -d postgres < ex1.sql
+```
---
+++ ex1.sql
@@ -0,0 +1,13 @@
+-- Which shippers do we have?
+-- We have a table called Shippers. Return all the fields from all the shippers 
+
+SELECT * FROM public.Shippers;
+
+/*
+ shipperid |   companyname    |     phone      
+-----------+------------------+----------------
+         1 | Speedy Express   | (503) 555-9831
+         2 | United Package   | (503) 555-3199
+         3 | Federal Shipping | (503) 555-9931
+(3 rows)
+*/
